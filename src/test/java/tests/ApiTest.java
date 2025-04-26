814d838979ff2650207fc7e9a4f9314e5ed9fbef
Oleg Namozov <oleg8494@yandex.ru>
2025-04-26 20:14:28 +0300

    new test

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -1,4 +1,22 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class ApiTest {
+
+    @Test
+    void checkTest() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("127.0"))
+                .statusCode(200);
+    }
 }
