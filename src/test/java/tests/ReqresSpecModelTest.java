bc01d7a2e8180e3d48250a9ba866a5691eb25e28
Oleg Namozov <oleg8494@yandex.ru>
2025-05-04 18:55:48 +0300

    del config browser

--- src/test/java/tests/ReqresSpecModelTest.java
+++ src/test/java/tests/ReqresSpecModelTest.java
@@ -24,19 +24,9 @@ public class ReqresSpecModelTest {
 @BeforeAll
 static void browserConfiguration() {
 
-    Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-    Configuration.browser = System.getProperty("browser", "chrome");
-    Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
-    Configuration.timeout = 2000;
     Configuration.remote = System.getProperty("browserRemote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
-    DesiredCapabilities capabilities = new DesiredCapabilities();
 
-    capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-            "enableVNC", true,
-            "enableVideo", true
-    ));
-    Configuration.browserCapabilities = capabilities;
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
 }
